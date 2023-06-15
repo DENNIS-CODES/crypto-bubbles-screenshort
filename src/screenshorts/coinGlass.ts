e23b6bf8815d4c6875559633540f6a9288ae1c88
Dennis-Codes <dennis@ngeni.io>
2023-06-15 23:38:59 +0300

    implemented dark mode

--- src/screenshorts/coinGlass.ts
+++ src/screenshorts/coinGlass.ts
@@ -2,21 +2,29 @@ import puppeteer from 'puppeteer';
 import cron from 'node-cron';
 import { sendPhotoToWhitelistedUsers } from '../telegram';
 
-// Set up the task
 export const task = cron.schedule('*/5 * * * *', async () => {
     // Start the browser and create a new page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const watchDog = page.waitForFunction('window.innerWidth > 1300');
     await page.setViewport({ width: 1920, height: 1080 });
-    await watchDog
+    await watchDog;
     // Navigate to the website
     await page.goto('https://www.coinglass.com/', { waitUntil: 'networkidle0' });
 
+    // Wait for a bit for the settings to take effect
+    await page.waitForTimeout(3000);
+
+    // Click on the div to switch to dark mode
+    const darkModeToggle = '.shou.MuiBox-root.cg-style-14d0z92';
+    await page.click(darkModeToggle);
+
+    // Wait for a bit for the dark mode to take effect
+    await new Promise(r => setTimeout(r, 3000));
+
     // Select the specific element to screenshot
     const element = await page.$('.mbh.MuiBox-root.cg-style-1mh511h');
-     // Wait for a bit for the settings to take effect
-     await new Promise(r => setTimeout(r, 3000));
+
     if (element) {
         // Step 4: Take a screenshot
         const screenshot: any = await page.screenshot({ fullPage: false });
@@ -26,3 +34,4 @@ export const task = cron.schedule('*/5 * * * *', async () => {
         await sendPhotoToWhitelistedUsers(screenshot);
     }
 });
+
