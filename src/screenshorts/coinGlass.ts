6ac97c0feaa5fac140a77969049ebeb43c02dc9e
Dennis-Codes <dennis@ngeni.io>
2023-06-16 22:27:33 +0300

    improved the quality of the image

--- src/screenshorts/coinGlass.ts
+++ src/screenshorts/coinGlass.ts
@@ -3,15 +3,10 @@ import cron from 'node-cron';
 import { sendPhotoToWhitelistedUsers } from '../telegram';
 
 export const task = cron.schedule('*/5 * * * *', async () => {
-    // Start the browser and create a new page
-    const browser = await puppeteer.launch({
-        headless: 'new',
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const watchDog = page.waitForFunction('window.innerWidth > 1300');
-    await page.setViewport({ width: 1920, height: 1080 });
-    await watchDog;
-    // Navigate to the website
+
+    await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
     await page.goto('https://www.coinglass.com/', { waitUntil: 'networkidle0' });
 
     // Wait for a bit for the settings to take effect
@@ -28,12 +23,9 @@ export const task = cron.schedule('*/5 * * * *', async () => {
     const element = await page.$('.mbh.MuiBox-root.cg-style-1mh511h');
 
     if (element) {
-        // Step 4: Take a screenshot
-        const screenshot: any = await page.screenshot({ fullPage: false });
-        // Step 5: Close the browser
+        // Take a screenshot of the selected element
+        const screenshot = await element.screenshot();
         await browser.close();
-        // Step 6: Send the screenshot to the Telegram chat
         await sendPhotoToWhitelistedUsers(screenshot);
     }
 });
-
