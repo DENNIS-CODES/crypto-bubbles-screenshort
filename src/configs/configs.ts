0da3832e163f42c61929ac6b256eab0d66e1546f
Dennis-Codes <dennis@ngeni.io>
2023-06-16 22:19:48 +0300

    added more users

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -2,6 +2,6 @@ import 'dotenv/config'
 
 export const configs = {
     BOT_TOKEN: process.env.BOT_TOKEN || "",
-    WHITELISTED_USERS: [1195869296],
+    WHITELISTED_USERS: [1195869296, 1433099385],
     TELEGRAM_DELETE_MESSAGE_INTERVAL: 10000,
 }
