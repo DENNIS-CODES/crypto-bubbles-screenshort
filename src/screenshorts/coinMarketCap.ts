6292315d56700608a25ec77141b14b913f00a642
Dennis-Codes <dennis@ngeni.io>
2023-06-18 03:25:29 +0300

    fixed taking screenshort for coin market cap

--- src/screenshorts/coinMarketCap.ts
+++ src/screenshorts/coinMarketCap.ts
@@ -2,33 +2,39 @@ import puppeteer from 'puppeteer';
 import cron from 'node-cron';
 import { sendMessage, sendPhotoToWhitelistedUsers } from '../telegram';
 
+
 export const CoinMarketTask = cron.schedule('*/15 * * * *', async () => {
-    const browser = await puppeteer.launch({
-        headless: 'new',
-    });
-    const page = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch({
+            headless: 'new', // Corrected launch option
+        });
+        const page = await browser.newPage();
 
-    await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
-    await page.goto('https://coinmarketcap.com/gainers-losers/', { waitUntil: 'networkidle0' });
+        await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
+        await page.goto('https://coinmarketcap.com/gainers-losers/', { waitUntil: 'networkidle0' });
 
-    // Wait for a bit for the settings to take effect
-    await new Promise(r => setTimeout(r, 3000));
+        // Wait for a bit for the settings to take effect
+        await new Promise(r => setTimeout(r, 3000));
 
-    // Click on the div to switch to dark mode
-    const darkModeToggle = 'button.sc-476bb07-0.sc-8aef48ae-6.bBHpMZ.cmc-theme-picker.cmc-theme-picker--night';
-    await page.click(darkModeToggle);
+        // Click on the div to switch to dark mode
+        const darkModeToggle = 'button.sc-476bb07-0.sc-8aef48ae-6.bBHpMZ.cmc-theme-picker.cmc-theme-picker--night';
+        await page.click(darkModeToggle);
 
-    // Wait for a bit for the dark mode to take effect
-    await new Promise(r => setTimeout(r, 3000));
+        // Wait for a bit for the dark mode to take effect
+        await new Promise(r => setTimeout(r, 3000));
 
-    // Select the specific element to screenshot
-    const element = await page.$('.sc-fbf6f08a-0.fvFzkg.table-wrap');
-
-    if (element) {
-        // Take a screenshot of the selected element
-        const screenshot = await element.screenshot();
+        // Select the specific element to screenshot
+        const element = await page.$('.sc-fbf6f08a-0.fvFzkg.table-wrap');
         sendMessage('CoinMarketCap');
-        await sendPhotoToWhitelistedUsers(screenshot);
+
+        if (element) {
+            // Take a screenshot of the selected element
+            const screenshot = await element.screenshot({ encoding: 'binary' }); // Added screenshot options
+            await sendPhotoToWhitelistedUsers(screenshot);
+        }
+
         await browser.close();
+    } catch (error) {
+        console.error(error); // Simple error handling
     }
 });
